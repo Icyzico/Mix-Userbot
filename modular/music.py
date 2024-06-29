dcc2ef73ff8e068e65380d9f2b0d67127849987e
jonesroot <jonesroot@users.noreply.github.com>
2024-06-29 10:31:16 +0000

    Auto Fix

--- modular/music.py
+++ modular/music.py
@@ -308,9 +308,8 @@ async def _(client: nlx, message):
     return
 
 
-from pyrogram.raw.base import InputPeer
-from pyrogram.raw.types import InputPeerSelf
 from pyrogram.raw.functions.phone import EditGroupCallParticipant
+from pyrogram.raw.types import InputPeerSelf
 
 from .vcs import get_group_call
 
@@ -327,8 +326,7 @@ async def _(client: nlx, message):
         return await message.reply(
             f"{em.gagal} Kalo mau set volume masukin angka dari 1 - 200!"
         )
-    
-    makspol = 200
+
     pol = int(message.command[1])
     group_call = await get_group_call(client, message, err_msg=", Kesalahan...")
     if not group_call:
@@ -338,7 +336,12 @@ async def _(client: nlx, message):
     try:
         await client.invoke(
             EditGroupCallParticipant(
-                call=group_call, participant=InputPeerSelf(), muted=False, volume=nihpol, video_stoped=False, video_paused=False
+                call=group_call,
+                participant=InputPeerSelf(),
+                muted=False,
+                volume=nihpol,
+                video_stoped=False,
+                video_paused=False,
             )
         )
         return await message.reply(f"{em.sukses} Volume berhasil diatur ke `{pol}%`")
