14ff260f5c9e6eb9de8da54f888a57e63f0736d1
Lucifer <38715155+jonesroot@users.noreply.github.com>
2024-06-29 23:48:10 +0700

    update modular/music.py

--- modular/music.py
+++ modular/music.py
@@ -349,7 +349,7 @@ async def _(client: nlx, message):
         return await message.reply(
             f"{em.sukses} Volume berhasil diatur ke `{message.command[1]}%`"
         )
-    except BaseException as e:
+    except Exception as e:
         return await message.reply(cgr("err").format(em.gagal, e))
 
 
