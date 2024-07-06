14e757fa4eeaabcbc490a4a46e4c5b2584209a68
jonesroot <jonesroot@users.noreply.github.com>
2024-07-06 05:48:47 +0000

    Auto Fix

--- Mix/mix_client.py
+++ Mix/mix_client.py
@@ -18,8 +18,8 @@ from pyrogram.enums import *
 from pyrogram.errors import *
 from pyrogram.handlers import *
 from pyrogram.types import *
-from team.nandev.class_modules import CMD_HELP
 from team.nandev.class_log import LOGGER
+from team.nandev.class_modules import CMD_HELP
 from team.nandev.database import ndB, udB
 
 from config import *
