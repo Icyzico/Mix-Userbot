18e52969741952ca90f1103c8905edf5415a43fe
jonesroot <jonesroot@users.noreply.github.com>
2024-07-06 04:46:13 +0000

    Auto Fix

--- modular/sudo.py
+++ modular/sudo.py
@@ -47,15 +47,16 @@ async def _(c: nlx, m):
         user = await c.get_users(user_id)
     except Exception as error:
         return await msg.edit(error)
-    sudoers = udB.get_list_from_var(c.me.id, "sudoers", "userid")
+    udB.get_list_from_var(c.me.id, "sudoers", "userid")
     usro = f"[{user.first_name} {user.last_name or ''}](tg://user?id={user.id})"
     if user.id not in sudo_users:
         return await msg.edit(cgr("sud_3").format(em.sukses, usro))
     try:
-      udB.remove_from_var(c.me.id, "sudoers", user.id, "userid")
-      return await msg.edit(cgr("sud_4").format(em.sukses, usro))
+        udB.remove_from_var(c.me.id, "sudoers", user.id, "userid")
+        return await msg.edit(cgr("sud_4").format(em.sukses, usro))
     except Exception as error:
         return await msg.edit(cgr("err").format(em.gagal, error))
+
 
 @ky.ubot("sudolist")
 async def _(c: nlx, m):
